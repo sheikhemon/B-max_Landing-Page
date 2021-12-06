59602443f58ff849bbc3234e5680add3aa5f6a1c
Emon Sheikh <sheikhemon@gmail.com>
2021-12-06 23:26:26 +0600

    Bmax Landing Page

---
+++ js/active.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.slide-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    $('.project-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+})
